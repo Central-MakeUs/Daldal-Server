502e9147c6792d30853e86e4122c63cadd56542a
KarmaPol <kkhkr98@gmail.com>
2024-01-18 21:49:17 +0900

    fix: infra rdb 모듈 삭제, 어노테이션만 추가

--- core/core-domain/src/main/java/com/mm/coredomain/CoreDomainConfig.java
+++ core/core-domain/src/main/java/com/mm/coredomain/CoreDomainConfig.java
@@ -8,8 +8,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EntityScan("com.pgms.coredomain")
-@EnableJpaRepositories("com.mm.coredomain")
 @EnableJpaAuditing
+@EnableTransactionManagement
+@EnableJpaRepositories("com.mm.coredomain")
 public class CoreDomainConfig {
 }
-
