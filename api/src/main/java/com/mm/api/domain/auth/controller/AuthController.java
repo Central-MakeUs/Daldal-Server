be4827e37603f4da8a24fb825f03268298e5b8cf
KarmaPol <kkhkr98@gmail.com>
2024-01-27 03:19:16 +0900

    fix: oauth2 로그인 주소 삭제

--- api/src/main/java/com/mm/api/domain/auth/controller/AuthController.java
+++ api/src/main/java/com/mm/api/domain/auth/controller/AuthController.java
@@ -1,5 +1,13 @@
 package com.mm.api.domain.auth.controller;
 
+import com.mm.api.domain.auth.dto.request.RefreshTokenRequest;
+import com.mm.api.domain.auth.dto.response.TokenResponse;
+import com.mm.api.domain.auth.service.AuthService;
+import com.mm.api.domain.member.dto.response.MemberInfoResponse;
+import com.mm.coresecurity.oauth.OAuth2UserDetails;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,46 +15,30 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.mm.api.domain.auth.dto.request.RefreshTokenRequest;
-import com.mm.api.domain.auth.dto.response.TokenResponse;
-import com.mm.api.domain.auth.service.AuthService;
-import com.mm.api.domain.member.dto.response.MemberInfoResponse;
-import com.mm.coresecurity.oauth.OAuth2UserDetails;
-
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
-
 @Tag(name = "회원 인증", description = "회원 인증 관련 API 입니다.")
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
-	private final AuthService authService;
+    private final AuthService authService;
 
-	@Operation(summary = "oAuth 로그인을 합니다. 현재 provider는 kakao만 제공됩니다.")
-	@GetMapping("/oauth2/authorization/{oauth2-provider}")
-	public void login() {
-		// oauth2 로그인
-	}
+    @Operation(summary = "로그아웃 합니다.")
+    @PostMapping("/api/v1/auth/logout")
+    public ResponseEntity<?> logout(@RequestBody RefreshTokenRequest request) {
+        authService.logout(request);
+        return ResponseEntity.noContent().build();
+    }
 
-	@Operation(summary = "로그아웃 합니다.")
-	@PostMapping("/api/v1/auth/logout")
-	public ResponseEntity<?> logout(@RequestBody RefreshTokenRequest request) {
-		authService.logout(request);
-		return ResponseEntity.noContent().build();
-	}
+    @Operation(summary = "로그인된 사용자 정보를 가져옵니다.")
+    @GetMapping("/api/v1/auth/me")
+    public ResponseEntity<?> getMe(@AuthenticationPrincipal OAuth2UserDetails userDetails) {
+        MemberInfoResponse response = authService.getMe(userDetails);
+        return ResponseEntity.ok(response);
+    }
 
-	@Operation(summary = "로그인된 사용자 정보를 가져옵니다.")
-	@GetMapping("/api/v1/auth/me")
-	public ResponseEntity<?> getMe(@AuthenticationPrincipal OAuth2UserDetails userDetails) {
-		MemberInfoResponse response = authService.getMe(userDetails);
-		return ResponseEntity.ok(response);
-	}
-
-	@Operation(summary = "access token을 갱신합니다.")
-	@PostMapping("/api/v1/auth/refresh-access-token")
-	public ResponseEntity<TokenResponse> refreshAccessToken(@RequestBody RefreshTokenRequest request) {
-		TokenResponse tokenResponse = authService.refreshAccessToken(request);
-		return ResponseEntity.ok(tokenResponse);
-	}
+    @Operation(summary = "access token을 갱신합니다.")
+    @PostMapping("/api/v1/auth/refresh-access-token")
+    public ResponseEntity<TokenResponse> refreshAccessToken(@RequestBody RefreshTokenRequest request) {
+        TokenResponse tokenResponse = authService.refreshAccessToken(request);
+        return ResponseEntity.ok(tokenResponse);
+    }
 }
