f5f0d265f3f5707ee40e30abc3ef66937cbc9619
KarmaPol <kkhkr98@gmail.com>
2024-01-28 18:16:38 +0900

    feat: #15 dib repo 추가

---
+++ core/core-domain/src/main/java/com/mm/coredomain/repository/DibRepository.java
@@ -0,0 +1,8 @@
+package com.mm.coredomain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mm.coredomain.domain.Dib;
+
+public interface DibRepository extends JpaRepository<Dib, Long> {
+}
