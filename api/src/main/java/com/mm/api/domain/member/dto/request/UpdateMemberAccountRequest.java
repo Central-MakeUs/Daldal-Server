a73c2cf0af9700d3ba9d191d7f6d5892179fd492
KarmaPol <kkhkr98@gmail.com>
2024-01-28 18:14:17 +0900

    feat: item category 추가

--- api/src/main/java/com/mm/api/domain/member/dto/request/UpdateMemberAccountRequest.java
+++ api/src/main/java/com/mm/api/domain/member/dto/request/UpdateMemberAccountRequest.java
@@ -1,4 +1,4 @@
 package com.mm.api.domain.member.dto.request;
 
-public record UpdateMemberAccountRequest(String account, String accountBank) {
+public record UpdateMemberAccountRequest(String depositorName, String account, String accountBank) {
 }
