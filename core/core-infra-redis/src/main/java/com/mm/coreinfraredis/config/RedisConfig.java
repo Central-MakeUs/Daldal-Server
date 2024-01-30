97b44b5e4a193fac65bd18e24beca5a5b6f0120e
KarmaPol <kkhkr98@gmail.com>
2024-01-30 21:26:57 +0900

    feat: #22 redis serializer 추가

--- core/core-infra-redis/src/main/java/com/mm/coreinfraredis/config/RedisConfig.java
+++ core/core-infra-redis/src/main/java/com/mm/coreinfraredis/config/RedisConfig.java
@@ -7,6 +7,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -20,6 +21,11 @@ public class RedisConfig {
 	@Bean
 	public RedisConnectionFactory redisConnectionFactory() {
 		return new LettuceConnectionFactory(redisHost, redisPort);
+	}
+
+	@Bean
+	public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
+		return new GenericJackson2JsonRedisSerializer();
 	}
 
 	@Bean
