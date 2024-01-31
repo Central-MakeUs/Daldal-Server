2ee5ff4a41b62b62c857ee9dfac0b1f1c50a5c19
KarmaPol <kkhkr98@gmail.com>
2024-01-31 23:58:39 +0900

    fix: redis 만료 시간 수정

--- core/core-infra-redis/src/main/java/com/mm/coreinfraredis/repository/RedisRefreshTokenRepository.java
+++ core/core-infra-redis/src/main/java/com/mm/coreinfraredis/repository/RedisRefreshTokenRepository.java
@@ -1,33 +1,32 @@
 package com.mm.coreinfraredis.repository;
 
-import java.util.Optional;
-import java.util.concurrent.TimeUnit;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Repository;
 
-import lombok.RequiredArgsConstructor;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 @Repository
 @RequiredArgsConstructor
 public class RedisRefreshTokenRepository {
-	private final RedisTemplate redisTemplate;
-	private static final long REFRESH_TOKEN_EXPIRE_LONG = 259200L;
+    private final RedisTemplate redisTemplate;
+    private static final long REFRESH_TOKEN_EXPIRE_LONG = 259200L;
 
-	public void save(String refreshToken, Long memberId) {
-		ValueOperations<String, Long> valueOperations = redisTemplate.opsForValue();
-		valueOperations.set(refreshToken, memberId);
-		redisTemplate.expire(refreshToken, 60L, TimeUnit.SECONDS);
-	}
+    public void save(String refreshToken, Long memberId) {
+        ValueOperations<String, Long> valueOperations = redisTemplate.opsForValue();
+        valueOperations.set(refreshToken, memberId);
+        redisTemplate.expire(refreshToken, REFRESH_TOKEN_EXPIRE_LONG, TimeUnit.SECONDS);
+    }
 
-	public void delete(String refreshToken) {
-		redisTemplate.delete(refreshToken);
-	}
+    public void delete(String refreshToken) {
+        redisTemplate.delete(refreshToken);
+    }
 
-	public Optional<Long> findByRefreshToken(String refreshToken) {
-		ValueOperations<String, Long> valueOperations = redisTemplate.opsForValue();
-		Long memberId = valueOperations.get(refreshToken);
-		return Optional.ofNullable(memberId);
-	}
+    public Optional<Long> findByRefreshToken(String refreshToken) {
+        ValueOperations<String, Long> valueOperations = redisTemplate.opsForValue();
+        Long memberId = valueOperations.get(refreshToken);
+        return Optional.ofNullable(memberId);
+    }
 }
