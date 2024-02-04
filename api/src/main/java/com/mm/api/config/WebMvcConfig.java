58db8a8d3eee50ae175f492e262e31796f1049fb
KarmaPol <kkhkr98@gmail.com>
2024-02-04 18:12:21 +0900

    fix: configuration 어노테이션 추가

--- api/src/main/java/com/mm/api/config/WebMvcConfig.java
+++ api/src/main/java/com/mm/api/config/WebMvcConfig.java
@@ -1,15 +1,17 @@
 package com.mm.api.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 			.allowedOrigins("/**")
 			.allowedHeaders("*")
-			.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+			.allowedMethods("HEAD", "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
 			.allowCredentials(true);
 	}
 }
