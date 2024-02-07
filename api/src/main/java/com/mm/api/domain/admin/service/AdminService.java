16c52efce8bc41dadb62adebe39cb6be2ebd8332
KarmaPol <kkhkr98@gmail.com>
2024-02-08 01:14:30 +0900

    feat: 어드민 추천 상품 등록/해제 기능 추가

--- api/src/main/java/com/mm/api/domain/admin/service/AdminService.java
+++ api/src/main/java/com/mm/api/domain/admin/service/AdminService.java
@@ -1,13 +1,5 @@
 package com.mm.api.domain.admin.service;
 
-import static com.mm.api.exception.ErrorCode.*;
-
-import java.util.List;
-import java.util.stream.IntStream;
-
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.mm.api.domain.admin.dto.response.AdminItemListResponse;
 import com.mm.api.domain.item.dto.response.ItemDetailResponse;
 import com.mm.api.exception.CustomException;
@@ -17,79 +9,95 @@ import com.mm.coredomain.domain.ItemVideo;
 import com.mm.coredomain.repository.ItemRepository;
 import com.mm.coreinfrafeign.crawler.service.CrawlerService;
 import com.mm.coreinfraqdsl.repository.AdminCustomRepository;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static com.mm.api.exception.ErrorCode.ITEM_NOT_FOUND;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class AdminService {
-	private final ItemRepository itemRepository;
-	private final CrawlerService crawlerService;
-	private final AdminCustomRepository adminCustomRepository;
+    private final ItemRepository itemRepository;
+    private final CrawlerService crawlerService;
+    private final AdminCustomRepository adminCustomRepository;
 
-	public void updateVideoUrl(Long itemId, String url) {
-		Item item = getItem(itemId);
-		ItemVideo itemVideo = ItemVideo.builder()
-			.url(url)
-			.item(item)
-			.build();
-		item.setItemVideos(List.of(itemVideo));
-	}
+    public void updateVideoUrl(Long itemId, String url) {
+        Item item = getItem(itemId);
+        ItemVideo itemVideo = ItemVideo.builder()
+                .url(url)
+                .item(item)
+                .build();
+        item.setItemVideos(List.of(itemVideo));
+    }
 
-	public ItemDetailResponse crawlItem(String url) {
-		Item item = crawlerService.getZigZagItemByCrawler(url);
-		Item savedItem = itemRepository.save(item);
-		return getItemDetailResponseByItem(savedItem);
-	}
+    public ItemDetailResponse crawlItem(String url) {
+        Item item = crawlerService.getZigZagItemByCrawler(url);
+        Item savedItem = itemRepository.save(item);
+        return getItemDetailResponseByItem(savedItem);
+    }
 
-	public AdminItemListResponse getItems(Integer page) {
-		List<Item> items = adminCustomRepository.getItemsByPage(page);
+    public AdminItemListResponse getItems(Integer page) {
+        List<Item> items = adminCustomRepository.getItemsByPage(page);
 
-		List<List<String>> itemImageLists = items.stream()
-			.map(item ->
-				item.getItemImages()
-					.stream()
-					.map(ItemImage::getUrl)
-					.toList())
-			.toList();
+        List<List<String>> itemImageLists = items.stream()
+                .map(item ->
+                        item.getItemImages()
+                                .stream()
+                                .map(ItemImage::getUrl)
+                                .toList())
+                .toList();
 
-		List<List<String>> itemVideoLists = items.stream()
-			.map(item ->
-				item.getItemVideos()
-					.stream()
-					.map(ItemVideo::getUrl)
-					.toList())
-			.toList();
+        List<List<String>> itemVideoLists = items.stream()
+                .map(item ->
+                        item.getItemVideos()
+                                .stream()
+                                .map(ItemVideo::getUrl)
+                                .toList())
+                .toList();
 
-		List<AdminItemListResponse.AdminItemResponse> adminItemResponses = IntStream.range(0, items.size())
-			.mapToObj(i ->
-				AdminItemListResponse.AdminItemResponse.of(items.get(i), itemImageLists.get(i), itemVideoLists.get(i)))
-			.toList();
+        List<AdminItemListResponse.AdminItemResponse> adminItemResponses = IntStream.range(0, items.size())
+                .mapToObj(i ->
+                        AdminItemListResponse.AdminItemResponse.of(items.get(i), itemImageLists.get(i), itemVideoLists.get(i)))
+                .toList();
 
-		Long pageNum = adminCustomRepository.getPageNum();
+        Long pageNum = adminCustomRepository.getPageNum();
 
-		return new AdminItemListResponse(pageNum, adminItemResponses);
-	}
+        return new AdminItemListResponse(pageNum, adminItemResponses);
+    }
 
-	private Item getItem(Long id) {
-		return itemRepository.findById(id)
-			.orElseThrow(() -> new CustomException(ITEM_NOT_FOUND));
-	}
+    public void updateItemSuggest(Long itemId) {
+        Item item = getItem(itemId);
+        item.setItemSuggested();
+    }
 
-	private ItemDetailResponse getItemDetailResponseByItem(Item savedItem) {
-		List<String> images = null;
-		if (savedItem.getItemImages() != null) {
-			images = savedItem.getItemImages().stream()
-				.map(ItemImage::getUrl)
-				.toList();
-		}
-		List<String> videos = null;
-		if (savedItem.getItemVideos() != null) {
-			videos = savedItem.getItemVideos().stream()
-				.map(ItemVideo::getUrl)
-				.toList();
-		}
-		return ItemDetailResponse.of(savedItem, images, videos, false);
-	}
+    public void updateItemNotSuggest(Long itemId) {
+        Item item = getItem(itemId);
+        item.setItemNotSuggested();
+    }
+
+    private Item getItem(Long id) {
+        return itemRepository.findById(id)
+                .orElseThrow(() -> new CustomException(ITEM_NOT_FOUND));
+    }
+
+    private ItemDetailResponse getItemDetailResponseByItem(Item savedItem) {
+        List<String> images = null;
+        if (savedItem.getItemImages() != null) {
+            images = savedItem.getItemImages().stream()
+                    .map(ItemImage::getUrl)
+                    .toList();
+        }
+        List<String> videos = null;
+        if (savedItem.getItemVideos() != null) {
+            videos = savedItem.getItemVideos().stream()
+                    .map(ItemVideo::getUrl)
+                    .toList();
+        }
+        return ItemDetailResponse.of(savedItem, images, videos, false);
+    }
 }
