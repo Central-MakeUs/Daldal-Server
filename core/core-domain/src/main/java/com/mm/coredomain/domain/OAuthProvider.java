eca772dc66a711f7d1293b44aceeaa7be5dac41a
KarmaPol <kkhkr98@gmail.com>
2024-02-13 02:35:52 +0900

    feat: 소셜 로그인 회원가입, 로그인 분기 추가

--- core/core-domain/src/main/java/com/mm/coredomain/domain/OAuthProvider.java
+++ core/core-domain/src/main/java/com/mm/coredomain/domain/OAuthProvider.java
@@ -1,5 +1,13 @@
 package com.mm.coredomain.domain;
 
 public enum OAuthProvider {
-	KAKAO, APPLE
+    KAKAO, APPLE;
+
+    public static OAuthProvider of(String input) {
+        try {
+            return OAuthProvider.valueOf(input.toUpperCase());
+        } catch (Exception e) {
+            throw new IllegalArgumentException("존재하지 소셜 로그인 타입입니다. : " + input);
+        }
+    }
 }
