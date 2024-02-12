1a9bac5790ddd041f9b40792b97d2d01404dc040
KarmaPol <kkhkr98@gmail.com>
2024-02-12 21:38:58 +0900

    fix: redis 세션 삭제

--- api/src/main/java/com/mm/api/ApiApplication.java
+++ api/src/main/java/com/mm/api/ApiApplication.java
@@ -2,14 +2,13 @@ package com.mm.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
-@EnableRedisHttpSession
+//@EnableRedisHttpSession
 @SpringBootApplication(scanBasePackages = {"com.mm"})
 public class ApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
 
 }
