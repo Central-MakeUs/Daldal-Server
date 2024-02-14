8e99fe6078891049898e663afbebad7476f1fd97
KarmaPol <kkhkr98@gmail.com>
2024-02-14 21:41:09 +0900

    fix: data sql group명 수정

---
+++ core/core-infra-rdb/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+insert into permission(id, name) values(1, 'ROLE_ADMIN');
+insert into permission(id, name) values(2, 'ROLE_USER');
+
+insert into member_groups(id, name) values(1, 'ADMIN_GROUP');
+insert into member_groups(id, name) values(2, 'USER_GROUP');
+
+insert into group_permission(id, member_groups_id, permission_id) values(1, 1, 1);
+insert into group_permission(id, member_groups_id, permission_id) values(2, 1, 2);
+insert into group_permission(id, member_groups_id, permission_id) values(3, 2, 2);
+
+insert into member(id, name, email, point, member_groups_id)
+values(1, 'test', 'test@test', 10000000, 1);
